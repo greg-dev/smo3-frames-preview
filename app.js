f0337194ea711e7ca9e3ccf662dcc9790350f95a
Greg Dev <greg.bak.dev@gmail.com>
2017-03-09 21:36:46 +0100

    Use Semistandard style

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-"use strict";
+'use strict';
 
-window.onload = function() {
+window.onload = function () {
   app.init('#app');
 };
 
 var app = {
-  domain: 'loadobfp.ru'.replace('obf','u','scated'),
+  domain: 'loadobfp.ru'.replace('obf', 'u', 'scated'),
 
   /* subdomains from frame2 to frame11 */
-  subdomains: [2,3,4,5,6,7,8,9,10,11].map(function(x) { return 'frame' + x; }),
+  subdomains: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(function (x) { return 'frame' + x; }),
 
-  urlTemplate: 'https://${SUBDOMAIN}.${DOMAIN}/${H}/${A}/${SH}.${IMAGE}.${SIZE}.jpg',
+  urlTemplate: 'https://[SUBDOMAIN].[DOMAIN]/[H]/[A]/[SH].[IMAGE].[SIZE].jpg',
 
-  getUrlRegex: function() {
+  getUrlRegex: function () {
     return '^.*(' + app.subdomains.join('|') + ').(' + app.domain + ')' +
       '\/([0-9a-f]{2})\/([0-9a-f]{2})\/([0-9]{7,8}).([0-9]{1,4}).([1-3]{1}.jpg)$';
   },
 
   sizes: [
     {values: '3|400', label: 'big'},
-    //{values: '2|100', label: 'medium'},
+    // {values: '2|100', label: 'medium'},
     {values: '1|80', label: 'small'}
   ],
 
   imgSize: 3,
   imgWidth: 400,
 
-  init: function(root) {
+  init: function (root) {
     var $container = document.createDocumentFragment();
 
     var $label = document.createElement('label');
@@ -39,7 +39,7 @@ var app = {
     $url.type = 'text';
     $url.placeholder = '//frame7.' + app.domain + '/c6/84/10473728.1.3.jpg';
     $url.value = '//frame7.' + app.domain + '/c6/84/10473728.5.3.jpg';
-    $url.addEventListener('change', function(){
+    $url.addEventListener('change', function () {
       app.updateUrl();
     });
     $container.appendChild($url);
@@ -73,7 +73,7 @@ var app = {
     $container.appendChild($end);
     app.$end = $end;
 
-    for(var i=0; i < app.sizes.length; i++) {
+    for (var i = 0; i < app.sizes.length; i++) {
       var $size = document.createElement('input');
       $size.name = 'size';
       $size.type = 'radio';
@@ -81,7 +81,7 @@ var app = {
       $size.value = app.sizes[i].values;
       $container.appendChild($size);
       $size.insertAdjacentText('afterend', app.sizes[i].label);
-      $size.addEventListener('change', function(el){
+      $size.addEventListener('change', function (el) {
         var values = el.target.value.split('|');
         app.imgSize = values[0];
         app.imgWidth = parseInt(values[1]);
@@ -90,8 +90,8 @@ var app = {
 
     var $submit = document.createElement('button');
     $submit.innerText = 'submit';
-    $submit.addEventListener('click', function(){
-      if (app.validateUrl()){
+    $submit.addEventListener('click', function () {
+      if (app.validateUrl()) {
         app.start();
       }
     });
@@ -101,7 +101,7 @@ var app = {
     var $stop = document.createElement('button');
     $stop.innerText = 'stop';
     $stop.disabled = true;
-    $stop.addEventListener('click', function(){
+    $stop.addEventListener('click', function () {
       app.stop();
     });
     $container.appendChild($stop);
@@ -113,7 +113,7 @@ var app = {
     app.$info = $info;
 
     var $pics = document.createElement('div');
-    $pics.addEventListener('click', function(){
+    $pics.addEventListener('click', function () {
       app.$zoom.style.display = 'none';
     });
     $container.appendChild($pics);
@@ -125,7 +125,7 @@ var app = {
     $zoom.style.width = 400;
     $zoom.style.height = 300;
     $zoom.style.display = 'none';
-    $zoom.addEventListener('click', function(){
+    $zoom.addEventListener('click', function () {
       app.$zoom.style.display = 'none';
     });
     $container.appendChild($zoom);
@@ -138,27 +138,27 @@ var app = {
     $root.appendChild($container);
   },
 
-  start: function() {
+  start: function () {
     app.$submit.disabled = true;
     app.$stop.disabled = false;
     app.clear();
     app.getImages();
   },
 
-  stop: function() {
+  stop: function () {
     app.$stop.disabled = true;
     app.$submit.disabled = false;
     return false;
   },
 
-  clear: function() {
+  clear: function () {
     var $pics = app.$pics;
     while ($pics.firstChild) {
       $pics.removeChild($pics.firstChild);
     }
   },
 
-  validateUrl: function() {
+  validateUrl: function () {
     var re = new RegExp(app.getUrlRegex());
     if (re.test(app.$url.value)) {
       return true;
@@ -168,9 +168,9 @@ var app = {
     }
   },
 
-  updateUrl: function() {
+  updateUrl: function () {
     var parts = app.$url.value.split(new RegExp(app.getUrlRegex()));
-    if (9 === parts.length) {
+    if (parts.length === 9) {
       app.$start.value = 1;
       app.$end.value = parts[6];
       return true;
@@ -180,14 +180,14 @@ var app = {
     }
   },
 
-  getFirstImageUrl: function() {
+  getFirstImageUrl: function () {
     var parts = app.$url.value.split(new RegExp(app.getUrlRegex()));
     app.url = app.urlTemplate
-      .replace('${DOMAIN}',parts[2])
-      .replace('${H}',parts[3])
-      .replace('${A}',parts[4])
-      .replace('${SH}',parts[5])
-      .replace('${SIZE}',app.imgSize);
+      .replace('[DOMAIN]', parts[2])
+      .replace('[H]', parts[3])
+      .replace('[A]', parts[4])
+      .replace('[SH]', parts[5])
+      .replace('[SIZE]', app.imgSize);
     app.currentImage = parseInt(app.$start.value);
     app.currentSubdomain = -1;
     app.lastImage = parseInt(app.$end.value);
@@ -195,11 +195,11 @@ var app = {
     return app.getNextImageUrl();
   },
 
-  getImages: function() {
+  getImages: function () {
     app.getImage(app.getFirstImageUrl());
   },
 
-  getNextImageUrl: function() {
+  getNextImageUrl: function () {
     app.currentSubdomain++;
     if (app.currentSubdomain >= app.subdomains.length) {
       app.currentSubdomain = 0;
@@ -211,20 +211,20 @@ var app = {
     }
     app.info(app.$info.textContent + '.');
     return app.url
-      .replace('${SUBDOMAIN}',app.subdomains[app.currentSubdomain])
-      .replace('${IMAGE}',app.currentImage);
+      .replace('[SUBDOMAIN]', app.subdomains[app.currentSubdomain])
+      .replace('[IMAGE]', app.currentImage);
   },
 
-  getImage: function(url) {
-    if(app.$stop.disabled) {
+  getImage: function (url) {
+    if (app.$stop.disabled) {
       return app.stop();
     }
     var $img = document.createElement('img');
-    $img.onload = function(x) {
-      if($img.naturalWidth === app.imgWidth) {
+    $img.onload = function (x) {
+      if ($img.naturalWidth === app.imgWidth) {
         $img.title = $img.src;
-        if(app.imgSize < 3) {
-          $img.addEventListener('mouseover', function(ev){
+        if (app.imgSize < 3) {
+          $img.addEventListener('mouseover', function (ev) {
             app.zoom(ev);
           });
         }
@@ -239,23 +239,23 @@ var app = {
         app.info('all done');
         return app.stop();
       }
-    }
+    };
     $img.src = url;
   },
 
-  zoom: function(ev) {
+  zoom: function (ev) {
     app.$zoom.style.display = 'block';
     app.$zoom.style.left = ev.clientX + 'px';
     app.$zoom.style.top = ev.clientY + 'px';
     app.$zoom.src = ev.target.src.replace(app.imgSize + '.jpg', '3.jpg');
   },
 
-  info: function(msg) {
+  info: function (msg) {
     console.log(msg);
     app.$info.textContent = msg;
   },
 
-  error: function(msg) {
+  error: function (msg) {
     console.log(msg);
   }
 };
